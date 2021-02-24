ffdc39beacfe29a9fe4d9b3ffe8f6876330f1a90
Mathias007 <mathias.najlepszy@gmail.com>
2021-02-24 20:23:03 +0100

    store form data using name attribute.

--- script.js
+++ script.js
@@ -41,10 +41,26 @@ function validateForm() {
     }
 }
 
+function storeFormData() {
+    const user = {
+        name: form.name.value,
+        phone: form.phone.value,
+        email: form.email.value,
+        website: form.website.value,
+        password: form.password.value,
+    };
+    // Do something with user data (temporarily - log them in console)
+    console.log(user);
+}
+
 function processFormData(e) {
     e.preventDefault();
     // Validate Form
     validateForm();
+    // Submit Data if Valid
+    if (isValid && passwordsMatch) {
+        storeFormData();
+    }
 }
 
 // Event Listener
